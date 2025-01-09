4725a0eccb0a28e7e4f6c0e240697dbfa1d0b893
FRC8117-PC01 <FRC8117@gmail.com>
2025-01-08 20:09:04 -0500

    Began Robot Map

--- src/main/java/frc/robot/Util/RobotMap.java
+++ src/main/java/frc/robot/Util/RobotMap.java
@@ -1,5 +1,41 @@
 package frc.robot.Util;
 
-public class RobotMap {
+public class RobotMap
+{
+    public static final class MAP_CONTROLLER_CLASS
+    {
+        public static final int MAIN_CONTROLLER_PORT = 0;
+        
+    }    
+
+    public static final class MAP_DRIVETRAIN
+    {
+        public static final int NAVX_CAN = 0;
+
+        //Front Left - Module 0
+        public static final int FRONT_LEFT_DRIVE_CAN = 0;
+
+        //Front Right - Module 1
+
+        //Back Left - Module 2
+
+        //Back Right - Module 3
+
+    }
+
+    /*
     
+    Elevator
+        2 motors left& right
+    Claw
+        1 pitch motor
+        1 Wheel motor
+    Algae
+        1 pitch motor
+        1 wheel motor
+    Climber
+        1 climb motoor
+        
+    */
+
 }
