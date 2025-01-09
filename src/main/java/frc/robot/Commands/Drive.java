2c2886ee4a63b80d39a2d5ae9aabb890cd257e14
Chad Perry <chad25309@gmail.com>
2025-01-08 19:24:08 -0500

    Added Folders and drive base files

---
+++ src/main/java/frc/robot/Commands/Drive.java
@@ -0,0 +1,10 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Drive extends Command {
+
+    public Drive() {
+
+    }
+}
---
+++ src/main/java/frc/robot/Subsystems/Drive/Module.java
@@ -0,0 +1,8 @@
+package frc.robot.Subsystems.Drive;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Module extends SubsystemBase
+{
+    
+}
