9a0fd48f7250380f7a30ee926c47d14b60a9ee1c
msi laptop <chad25309@gmail.com>
2025-01-28 11:35:29 -0500

    clean l3 state made somewhat

--- src/main/java/frc/robot/Commands/States/CleanL3State.java
+++ src/main/java/frc/robot/Commands/States/CleanL3State.java
@@ -1,8 +1,69 @@
 package frc.robot.Commands.States;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Commands.Drive;
+import frc.robot.Subsystems.AlgaeRollers;
+import frc.robot.Subsystems.Climber;
+import frc.robot.Subsystems.Elevator;
+import frc.robot.Subsystems.Lights;
+import frc.robot.Subsystems.StateMachine;
+import frc.robot.Subsystems.StateMachine.RobotState;
+import frc.robot.Util.RobotMap.MAP_PWM_LIGHTS;
+import frc.robot.Subsystems.Vision;
 
-public class CleanL3State extends Command
+
+ // this is nathan, I'm making this state from what I know and looking
+ //  at the other already made states. for the packages, I don't know which
+ //  ones are necessary and which ones aren't. If you know what's redundant
+ //  what's not, please go ahead and change it
+public class CleanL3State extends Command //TODO
 {
-    
+    StateMachine s_StateMachine;
+    Drive c_Drive;
+    Elevator s_Elevator;
+    Climber s_Climber;
+    AlgaeRollers s_Rollers;
+    Lights s_Lights;
+    Vision s_Vision;
+
+        public CleanL3State(StateMachine s_StateMachine, Drive c_Drive, Elevator s_Elevator, Climber s_Climber, AlgaeRollers s_Rollers, Vision s_Vision, Lights s_Lights)
+    {
+        this.s_StateMachine = s_StateMachine;
+        this.c_Drive = c_Drive;
+        this.s_Elevator = s_Elevator;
+        this.s_Climber = s_Climber;
+        this.s_Rollers = s_Rollers;
+        this.s_Lights = s_Lights;
+        this.s_Vision = s_Vision;
+
+        addRequirements(s_StateMachine);
+    }
+    // set up
+    @Override
+    public void initialize()
+    {
+            s_StateMachine.setRobotState(RobotState.CLEAN_L3);
+            s_Lights.setNumber(MAP_PWM_LIGHTS.PWM_CORAL_COLOR);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    // loop
+    @Override
+    public void execute()
+    {
+
+    }
+     // Called once the command ends or is interrupted.
+     public void end(boolean interrupted)
+     {
+
+     }
+
+     // Returns true whe nthe comand should end.
+     @Override
+     public boolean isFInished()
+        {
+         return combo;
+        }
+     
 }
--- src/main/java/frc/robot/Util/RobotMap.java
+++ src/main/java/frc/robot/Util/RobotMap.java
@@ -68,26 +68,49 @@ public class RobotMap
     }
     public static final class MAP_APRIL_TAGS
     {
+         //[id num, height in inches, coordinate x, coordinate y, heading] inches to meters; use field manual image for id reference
+    public static final double[] RED_CS_X = {1, 1.4859, 16.6992, 0.6553, 126};
+    public static final double[] RED_CS_Y = {2, 1.4859, 16.6992, 7.4048, 234};
+    public static final double[] RED_PROCESSOR = {3, 1.3017, 11.6087, 8.0641, 270};
+    public static final double[] RED_SIDE_BLUE_BARGE = {4, 1.8678, 9.2800, 6.1340, 0};
+    public static final double[] RED_SIDE_RED_BARGE = {5, 1.8678, 9.2800, 1.9148, 0};
+    public static final double[] RED_REEF_KL = {6, 0.3081, 13.4742, 3.3074, 300};
+    public static final double[] RED_REEF_AB = {7, 0.3081, 13.8934, 4.0262, 0};
+    public static final double[] RED_REEF_CD = {8, 0.3081, 13.4742, 4.7449, 60};
+    public static final double[] RED_REEF_EF = {9, 0.3081, 12.6464, 4.7449, 120};
+    public static final double[] RED_REEF_GH = {10, 0.3081, 12.2194, 4.0262, 180};
+    public static final double[] RED_REEF_IJ = {11, 0.3081, 12.6464, 3.3074, 240};
+    public static final double[] BLUE_CS_Y = {12, 1.4859, 0.8507, 0.6553, 54};
+    public static final double[] BLUE_CS_X = {13, 1.4859, 0.8507, 7.4048, 306};
+    public static final double[] BLUE_PROCESSOR = {14, 1.8678, 8.2744, 6.1340, 0};
+    public static final double[] BLUE_SIDE_BLUE_BARGE = {15, 1.8678, 8.2744, 1.9148, 180};
+    public static final double[] BLUE_SIDE_RED_BARGE = {16, 1.3017, 5.9915, -0.0004, 90};
+    public static final double[] BLUE_REEF_CD = {17, 0.3081, 4.0734, 3.3074, 240};
+    public static final double[] BLUE_REEF_AB = {18, 0.3081, 3.6571, 4.0262, 0};
+    public static final double[] BLUE_REEF_KL = {19, 0.3081, 4.0734, 4.7449, 120};
+    public static final double[] BLUE_REEF_IJ = {20, 0.3081, 4.9068, 4.7449, 180};
+    public static final double[] BLUE_REEF_GH = {21, 0.3081, 5.3246, 5.3246, 0};
+    public static final double[] BLUE_REEF_EF = {22, 0.3081, 4.9068, 3.3074, 300};
         
     }
     public static final class MAP_PWM_LIGHTS
     {   // 
         // MAKE PREP STATES A PATTERN OF THEIR PARENT STATE (IF PWM_NONE IS GRAY, MAKE PWM_PREP_NONE FLASHING GRAY OR SOMETHING)
-        public static final double PWM_NONE_COLOR = 0;
-        public static final double PWM_SOURCE_COLOR = 0;
+        public static final double PWM_NONE_COLOR = 0; 
+        public static final double PWM_CS_COLOR = 0;
         public static final double PWM_INTAKE_ALGAE_COLOR = 0;
-        public static final double PWM_CORAL_COLOR = 0.85; // DARK BLUE
-        public static final double PWM_ALGAE_COLOR = 0.75; // DARK GREEN
-        public static final double PWM_COMBO_COLOR = 0.79; // BLUE GREEN
+        public static final double PWM_CORAL_COLOR = -0.01; // COLOR 1 LARSON SCANNER
+        public static final double PWM_ALGAE_COLOR = 0.19; // COLOR 2 LARSON SCANNER
+        public static final double PWM_COMBO_COLOR = 0.41; // COLOR 1 AND 2 COLOR GRADIENT
         public static final double PWM_PREP_L1_PATTERN = 0.59;
         public static final double PWM_PREP_L2_PATTERN = 0.69;
         public static final double PWM_PREP_L3_PATTERN = 0.33;
         public static final double PWM_PREP_L4_PATTERN = 0.21;
-        public static final double PWM_PREP_NONE_PATTERN = 0;// NONE COLOR LARSON SCANNER
-        public static final double PWM_PREP_ALGAE_PATTERN = 0;// GREEN LARSON SCANNER
+        public static final double PWM_PREP_NONE_PATTERN = 0;
+        public static final double PWM_PREP_ALGAE_PATTERN = 0;
         public static final double PWM_CLEAN_L2_PATTERN = 0;
         public static final double PWM_CLEAN_L3_PATTERN = 0;
-        public static final double PWM_CLIMBING_COLOR = 0;
+        public static final double PWM_CLIMBING_COLOR = 0.93; // SOLID WHITE
 
         
 
