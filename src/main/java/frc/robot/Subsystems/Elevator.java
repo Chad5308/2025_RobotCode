1577ea5427029197b9de03b33014d9dd974ccc5c
FRC8117-PC01 <FRC8117@gmail.com>
2025-01-29 18:18:09 -0500

    Updated elevator subsystem

--- src/main/java/frc/robot/Subsystems/Elevator.java
+++ src/main/java/frc/robot/Subsystems/Elevator.java
@@ -1,11 +1,9 @@
 package frc.robot.Subsystems;
-
-import com.ctre.phoenix6.configs.FeedbackConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
-import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.swerve.SwerveModuleConstants.ClosedLoopOutputType;
+// import com.ctre.phoenix6.configs.FeedbackConfigs;
+// import com.ctre.phoenix6.configs.Slot0Configs;
+// import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+// import com.ctre.phoenix6.controls.NeutralOut;
+// import com.ctre.phoenix6.swerve.SwerveModuleConstants.ClosedLoopOutputType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -24,16 +22,21 @@ import frc.robot.Util.RobotMap.MAP_ELEVATOR;
 
 public class Elevator extends SubsystemBase
 {
-    public TalonFX ELE_LEFT;
-    public TalonFX ELE_RIGHT;
-    
-    public MotionMagicVelocityVoltage motionMagicRequest;
-    // public TalonFXConfiguration elevatorConfig = new TalonFXConfiguration();
-    public NeutralOut neutralOut;
-    public Slot0Configs ELE_CONFIG;
-    public FeedbackConfigs ELE_FEEDBACKCONFIG;
-    public ClosedLoopOutputType elevatorClosedLoopOutput = ClosedLoopOutputType.Voltage;
+    // Left = Right
+    public SparkMax ELE_LEFT;
+    public RelativeEncoder ELE_LEFT_ENCODER;
+    public SparkClosedLoopController ELE_LEFT_PID;
 
+    public SparkMax ELE_RIGHT;
+    public RelativeEncoder ELE_RIGHT_ENCODER;
+    public SparkClosedLoopController ELE_RIGHT_PID;
+
+    // public MotionMagicVelocityVoltage motionMagicRequest;
+    // public SparkMaxConfiguration elevatorConfig = new SparkMaxConfiguration();
+    // public NeutralOut neutralOut;
+    // public Slot0Configs ELE_CONFIG;
+    // public FeedbackConfigs ELE_FEEDBACKCONFIG;
+    // public ClosedLoopOutputType elevatorClosedLoopOutput = ClosedLoopOutputType.Voltage;
 
     public SparkMax ELE_ARM;
     public RelativeEncoder ELE_ARM_ENCODER;
@@ -45,30 +48,41 @@ public class Elevator extends SubsystemBase
 
     public Elevator()
     {
-        ELE_LEFT = new TalonFX(MAP_ELEVATOR.ELEVATOR_LEFT);
-        ELE_RIGHT = new TalonFX(MAP_ELEVATOR.ELEVATOR_RIGHT);
+        // ELE_LEFT = new TalonFX(MAP_ELEVATOR.ELEVATOR_LEFT);
+        // ELE_RIGHT = new TalonFX(MAP_ELEVATOR.ELEVATOR_RIGHT);
 
-        ELE_CONFIG = new Slot0Configs().withKP(0.1).withKI(0).withKD(0).withKS(0.5).withKV(0.2);
-        ELE_FEEDBACKCONFIG = new FeedbackConfigs().withRotorToSensorRatio(constants_Elevator.ELEVATOR_TO_INCHES);
+        // ELE_CONFIG = new Slot0Configs().withKP(0.1).withKI(0).withKD(0).withKS(0.5).withKV(0.2);
+        // ELE_FEEDBACKCONFIG = new FeedbackConfigs().withRotorToSensorRatio(constants_Elevator.ELEVATOR_TO_INCHES);
         
-        ELE_LEFT.getConfigurator().apply(ELE_CONFIG);
-        ELE_LEFT.getConfigurator().apply(ELE_FEEDBACKCONFIG);
+        // ELE_LEFT.getConfigurator().apply(ELE_CONFIG);
+        // ELE_LEFT.getConfigurator().apply(ELE_FEEDBACKCONFIG);
 
-        ELE_RIGHT.getConfigurator().apply(ELE_CONFIG);
-        ELE_RIGHT.getConfigurator().apply(ELE_FEEDBACKCONFIG);
+        // ELE_RIGHT.getConfigurator().apply(ELE_CONFIG);
+        // ELE_RIGHT.getConfigurator().apply(ELE_FEEDBACKCONFIG);
 
         config = new SparkMaxConfig().apply(new ClosedLoopConfig().pidf(0.0075, 0.0, 0.075, 0.0, ClosedLoopSlot.kSlot0));
         ELE_ARM = new SparkMax(MAP_ELEVATOR.ELEVATOR_RIGHT, MotorType.kBrushless);
         ELE_ARM_ENCODER = ELE_ARM.getEncoder();
         ELE_ARM_PID = ELE_ARM.getClosedLoopController();
         ELE_ARM.configure(config, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        
+
+        config = new SparkMaxConfig().apply(new ClosedLoopConfig().pidf(0.0075, 0.0, 0.075, 0.0, ClosedLoopSlot.kSlot0));
+        ELE_LEFT= new SparkMax(MAP_ELEVATOR.ELEVATOR_RIGHT, MotorType.kBrushless);
+        ELE_LEFT_ENCODER = ELE_ARM.getEncoder();
+        ELE_LEFT_PID = ELE_ARM.getClosedLoopController();
+        ELE_LEFT.configure(config, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        config = new SparkMaxConfig().apply(new ClosedLoopConfig().pidf(0.0075, 0.0, 0.075, 0.0, ClosedLoopSlot.kSlot0));
+        ELE_RIGHT= new SparkMax(MAP_ELEVATOR.ELEVATOR_RIGHT, MotorType.kBrushless);
+        ELE_RIGHT_ENCODER = ELE_ARM.getEncoder();
+        ELE_RIGHT_PID = ELE_ARM.getClosedLoopController();
+        ELE_RIGHT.configure(config, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);        
     }
 
 
     public double getPosition()
     {
-        return 0;
+        return ELE_LEFT_ENCODER.getPosition();
     }
     
     public boolean getGamePieceStored()
@@ -77,10 +91,10 @@ public class Elevator extends SubsystemBase
         //TODO Code this distance/proxy sensor in
     }
 
-    public void setElevatorPosition(ElevatorPositionGroup position)
-    {
-        
-    }
+    // public void setElevatorPosition(ElevatorPositionGroup position)
+    // {
+    //     return ELE_LEFT_ENCODER.setPosition(positi);
+    // }
 
 
      @Override
